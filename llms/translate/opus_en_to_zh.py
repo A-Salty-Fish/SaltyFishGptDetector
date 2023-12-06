dce74e3cf696b88a531350e0f34048b105ab2343
A-Salty-Fish <1309032417@qq.com>
2023-12-06 17:04:44 +0800

    refactor: translatro

--- llms/translate/opus_en_to_zh.py
+++ llms/translate/opus_en_to_zh.py
@@ -1,6 +1,18 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
-pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-zh")
 
-print(pipe("translate english to chinese"))
+def init_translator():
+
+    pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-zh")
+
+    return pipe
+
+
+def translate(translator, context):
+    return translator(context)
+
+if __name__ == '__main__':
+    translator = init_translator()
+
+    print(translate(translator, "translate english to chinese"))
--- llms/translate/opus_zh_to_en.py
+++ llms/translate/opus_zh_to_en.py
@@ -1,6 +1,18 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
-pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-zh-en")
 
-print(pipe("将中文翻译成英文"))
+def init_translator():
+
+    pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-zh-en")
+
+    return pipe
+
+
+def translate(translator, context):
+    return translator(context)
+
+if __name__ == '__main__':
+    translator = init_translator()
+
+    print(translate(translator, "将中文翻译成英文"))
