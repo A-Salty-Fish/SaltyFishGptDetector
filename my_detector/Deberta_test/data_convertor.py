fe2da7350a6f89c7943536f9b215c3209a80b386
A-Salty-Fish <1309032417@qq.com>
2023-12-22 15:19:56 +0800

    feat: cheat test data

--- my_detector/Deberta_test/data_convertor.py
+++ my_detector/Deberta_test/data_convertor.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 
 def convert_hc3_dataset_to_train_and_test(row_path, row_name ,target_path, total_num, train_rate):
@@ -47,12 +48,73 @@ def merge_hc3_dataset(target_path):
         out_train_file.write(json.dumps(train_list))
 
 
+
+def convert_CHEAT_dataset_to_train_and_test(row_path, row_name, target_path, total_num, train_rate):
+    i = 0
+    train_list = []
+    test_list = []
+    with open(row_path + row_name, 'r', encoding='utf-8') as in_file_1:
+        for line in in_file_1:
+            i+=1
+            if i > total_num:
+                break
+            json_obj = json.loads(line)
+            gpt_obj = {}
+            gpt_obj['label'] = 1
+            gpt_obj['content'] = json_obj['abstract']
+            if i <= total_num * train_rate:
+                train_list.append(gpt_obj)
+            else:
+                test_list.append(gpt_obj)
+    i = 0
+    with open(row_path + 'ieee-init.jsonl', 'r', encoding='utf-8') as in_file_2:
+        for line in in_file_2:
+            i += 1
+            if i > total_num:
+                break
+            json_obj = json.loads(line)
+            human_obj = {}
+            human_obj['label'] = 0
+            human_obj['content'] = json_obj['abstract']
+            if i <= total_num * train_rate:
+                train_list.append(human_obj)
+            else:
+                test_list.append(human_obj)
+    with open(target_path + row_name + '.test', 'w', encoding='utf-8') as out_test_file:
+        out_test_file.write(json.dumps(test_list))
+    with open(target_path + row_name + '.train', 'w', encoding='utf-8') as out_train_file:
+        out_train_file.write(json.dumps(train_list))
+
+
+def merge_CHEAT_dataset(target_path):
+    test_list = []
+    train_list = []
+    for file in  os.listdir(target_path):
+        with open(target_path + file, 'r', encoding='utf-8') as f:
+            arr = json.load(f)
+            if file.find('test') != -1:
+                for obj in arr:
+                    test_list.append(obj)
+            else:
+                for obj in arr:
+                    train_list.append(obj)
+    with open(target_path + "cheat_all.jsonl.test", 'w', encoding='utf-8') as out_test_file:
+        out_test_file.write(json.dumps(test_list))
+    with open(target_path + "cheat_all.jsonl.train", 'w', encoding='utf-8') as out_train_file:
+        out_train_file.write(json.dumps(train_list))
+
+
 if __name__ == '__main__':
 
-    finance_total = 3600
-    medicine_total = 1200
-    wiki_total = 800
+    # finance_total = 3600
+    # medicine_total = 1200
+    # wiki_total = 800
     # convert_hc3_dataset_to_train_and_test('../../data_collector/test_data/hc3_english/', 'finance.jsonl', './data/', finance_total, 0.2)
     # convert_hc3_dataset_to_train_and_test('../../data_collector/test_data/hc3_english/', 'medicine.jsonl', './data/', medicine_total, 0.2)
     # convert_hc3_dataset_to_train_and_test('../../data_collector/test_data/hc3_english/', 'wiki_csai.jsonl', './data/', wiki_total, 0.2)
-    merge_hc3_dataset('./data/')
+    # merge_hc3_dataset('./data/')
+    total = 4000
+    convert_CHEAT_dataset_to_train_and_test('../../data_collector/test_data/CHEAT/', 'ieee-chatgpt-fusion.jsonl', './data/', total, 0.2)
+    convert_CHEAT_dataset_to_train_and_test('../../data_collector/test_data/CHEAT/', 'ieee-chatgpt-generation.jsonl', './data/', total, 0.2)
+    convert_CHEAT_dataset_to_train_and_test('../../data_collector/test_data/CHEAT/', 'ieee-chatgpt-polish.jsonl', './data/', total, 0.2)
+    merge_CHEAT_dataset('./data/')
--- my_detector/Deberta_test/deberta_test.py
+++ my_detector/Deberta_test/deberta_test.py
@@ -37,7 +37,7 @@ def test_accurate(model, tokenizer, name):
         json_array = json.load(test_input)
         for json_obj in json_array:
             i += 2
-            print('test process : %s [%d/%d]' % (str(i / len(json_array)) + '%', i, len(json_array)), end='\r')
+            print('test process : %s [%d/%d]' % (str(i / len(json_array)*2) + '%', i, len(json_array) * 2), end='\r')
             raw_input = json_obj['content']
             raw_label = json_obj['label']
             inputs = tokenizer([raw_input], padding=True, truncation=True, return_tensors="pt").to(DEVICE)
@@ -61,26 +61,124 @@ def test_accurate(model, tokenizer, name):
             "ai_acc": (1.0 * (gpt_correct) / (gpt_total))
         }
 
+
+def test_all():
+    model, tokenizer = init_model_and_tokenizer('hc3_all')
+    human_correct = 0
+    human_total = 0
+    gpt_correct = 0
+    gpt_total = 0
+    i = 0
+    with open('./data/' + 'finance' + '.jsonl.test', 'r', encoding='utf-8') as test_input:
+        json_array = json.load(test_input)
+        for json_obj in json_array:
+            i += 2
+            print('test process : %s [%d/%d]' % (str(i / len(json_array)) + '%', i, len(json_array)), end='\r')
+            raw_input = json_obj['content']
+            raw_label = json_obj['label']
+            inputs = tokenizer([raw_input], padding=True, truncation=True, return_tensors="pt").to(DEVICE)
+            outputs = model(**inputs)
+            pred_labels = outputs.logits.cpu().argmax(-1).numpy()
+            pred_label = pred_labels[0]
+
+            if raw_label == 0:
+                human_total += 1
+                if pred_label == 0:
+                    human_correct += 1
+            else:
+                gpt_total += 1
+                if pred_label == 1:
+                    gpt_correct += 1
+
+        print({
+            "name": 'finance',
+            "total_acc": (1.0 * (human_correct + gpt_correct) / (human_total + gpt_total)),
+            "human_acc": (1.0 * human_correct / human_total),
+            "ai_acc": (1.0 * (gpt_correct) / (gpt_total))
+        })
+    human_correct = 0
+    human_total = 0
+    gpt_correct = 0
+    gpt_total = 0
+    i = 0
+    with open('./data/' + 'medicine' + '.jsonl.test', 'r', encoding='utf-8') as test_input:
+        json_array = json.load(test_input)
+        for json_obj in json_array:
+            i += 2
+            print('test process : %s [%d/%d]' % (str(i / len(json_array)) + '%', i, len(json_array)), end='\r')
+            raw_input = json_obj['content']
+            raw_label = json_obj['label']
+            inputs = tokenizer([raw_input], padding=True, truncation=True, return_tensors="pt").to(DEVICE)
+            outputs = model(**inputs)
+            pred_labels = outputs.logits.cpu().argmax(-1).numpy()
+            pred_label = pred_labels[0]
+
+            if raw_label == 0:
+                human_total += 1
+                if pred_label == 0:
+                    human_correct += 1
+            else:
+                gpt_total += 1
+                if pred_label == 1:
+                    gpt_correct += 1
+
+        print({
+            "name": 'medicine',
+            "total_acc": (1.0 * (human_correct + gpt_correct) / (human_total + gpt_total)),
+            "human_acc": (1.0 * human_correct / human_total),
+            "ai_acc": (1.0 * (gpt_correct) / (gpt_total))
+        })
+    human_correct = 0
+    human_total = 0
+    gpt_correct = 0
+    gpt_total = 0
+    i = 0
+    with open('./data/' + 'wiki_csai' + '.jsonl.test', 'r', encoding='utf-8') as test_input:
+        json_array = json.load(test_input)
+        for json_obj in json_array:
+            i += 2
+            print('test process : %s [%d/%d]' % (str(i / len(json_array)) + '%', i, len(json_array)), end='\r')
+            raw_input = json_obj['content']
+            raw_label = json_obj['label']
+            inputs = tokenizer([raw_input], padding=True, truncation=True, return_tensors="pt").to(DEVICE)
+            outputs = model(**inputs)
+            pred_labels = outputs.logits.cpu().argmax(-1).numpy()
+            pred_label = pred_labels[0]
+
+            if raw_label == 0:
+                human_total += 1
+                if pred_label == 0:
+                    human_correct += 1
+            else:
+                gpt_total += 1
+                if pred_label == 1:
+                    gpt_correct += 1
+
+        print({
+            "name": 'wiki_csai',
+            "total_acc": (1.0 * (human_correct + gpt_correct) / (human_total + gpt_total)),
+            "human_acc": (1.0 * human_correct / human_total),
+            "ai_acc": (1.0 * (gpt_correct) / (gpt_total))
+        })
+
 def test(name):
     model, tokenizer = init_model_and_tokenizer(name)
     return test_accurate(model, tokenizer, name)
 
 
 if __name__ == '__main__':
+    # test_all()
     print(test('medicine'))
-    print(test('finance'))
-    print(test('wiki_csai'))
-    print(test('hc3_all'))
+    # print(test('finance'))
+    # print(test('wiki_csai'))
+    # print(test('hc3_all'))
 #
-# load model:./finance/checkpoint-1600
-# cuda
-# load model successful: 2.0808396339416504
+# {'name': 'medicine', 'total_acc': 0.9958376690946931, 'human_acc': 0.9937565036420395, 'ai_acc': 0.9979188345473465}
 # {'name': 'finance', 'total_acc': 0.9908018049288442, 'human_acc': 0.984380423464075, 'ai_acc': 0.9972231863936133}
-# load model:./wiki_csai/checkpoint-200
-# cuda
-# load model successful: 2.097062826156616
 # {'name': 'wiki_csai', 'total_acc': 0.9695787831513261, 'human_acc': 0.9469578783151326, 'ai_acc': 0.9921996879875195}
-# load model:./hc3_all/checkpoint-4200
-# cuda
-# load model successful: 2.371985673904419
 # {'name': 'hc3_all', 'total_acc': 0.9792549631942895, 'human_acc': 0.9616328351550301, 'ai_acc': 0.996877091233549}
+
+#hc3 all
+# {'name': 'finance', 'total_acc': 0.9849010760152724, 'human_acc': 0.9732731690385283, 'ai_acc': 0.9965289829920166}
+# {'name': 'medicine', 'total_acc': 0.9963579604578564, 'human_acc': 0.9927159209157128, 'ai_acc': 1.0}
+# {'name': 'wiki_csai', 'total_acc': 0.9282371294851794, 'human_acc': 0.8627145085803433, 'ai_acc': 0.9937597503900156}
