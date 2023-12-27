952b240be3b69a3422a82a17312959389d381475
A-Salty-Fish <1309032417@qq.com>
2023-12-27 16:55:48 +0800

    feat: mistral chat

--- llms/chat/mistral_chat.py
+++ llms/chat/mistral_chat.py
@@ -1,18 +1,21 @@
+import time
+
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 device = "cuda" # the device to load the model onto
 
 def init_model_and_tokenizer():
-
+    start_time = time.time()
     model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
     tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
 
+    end_time = time.time()
+    print("load model successful: " + str(end_time - start_time))
     return model, tokenizer
 
 
 def chat(model, tokenizer, context):
-
-
+    start_time = time.time()
     messages = [
         {"role": "user", "content": context}
         # {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
@@ -26,9 +29,11 @@ def chat(model, tokenizer, context):
 
     generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
     decoded = tokenizer.batch_decode(generated_ids)
-    return decoded[0]
+    end_time = time.time()
+    print("generate response successful: " + str(end_time - start_time))
+    return decoded[0].split('[/INST]')[1].replace('</s>', '')
 
 
 if __name__ == '__main__':
     model, tokenizer = init_model_and_tokenizer()
-    print(chat(model, tokenizer, "Do you have mayonnaise recipes?"))
+    print(chat(model, tokenizer, "Please rewrite the following paragraphs to keep the original meaning intact and prohibit the output of irrelevant content: As the Web matures, an increasing number of dynamic information sources and services come online. Unlike static Web pages, these resources generate their contents dynamically in response to a query. They can be HTML-based, searching the site via an HTML form, or be a Web service. Proliferation of such resources has led to a number of novel applications, including Web-based mashups, such as Google maps and Yahoo pipes, information integration"))
