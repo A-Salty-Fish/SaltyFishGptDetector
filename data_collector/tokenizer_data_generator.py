5173f374877f201b2d033eb3c9b4553686411057
A-Salty-Fish <1309032417@qq.com>
2023-12-28 14:11:26 +0800

    feat: tokenizer data generate

--- data_collector/tokenizer_data_generator.py
+++ data_collector/tokenizer_data_generator.py
@@ -1,6 +1,8 @@
+import json
+
 from transformers import AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained('microsoft/deberta-v3-small', model_max_length=512)
+tokenizer = AutoTokenizer.from_pretrained('microsoft/deberta-v3-small', model_max_length=2048)
 
 from transformers import pipeline
 
@@ -18,8 +20,62 @@ def fill_mask(masked_text, pre_word = ''):
             return result['token_str']
     return pre_word
 
+def convert_hc3_english_to_tokens():
+    with open('./test_data/hc3_english/finance.jsonl', 'r', encoding='utf-8') as finance_file, \
+            open('./test_data/hc3_english/medicine.jsonl', 'r', encoding='utf-8') as medicine_file, \
+            open('./test_data/hc3_english/wiki_csai.jsonl', 'r', encoding='utf-8') as wiki_file:
+        with open('./finance_tokens.jsonl', 'a', encoding='utf-8') as finance_out_file:
+            for line in finance_file:
+                try:
+                    json_obj = json.loads(line)
+                    human_obj = {}
+                    gpt_obj = {}
+                    human_obj['row_label'] = 0
+                    gpt_obj['row_label'] = 1
+                    human_obj['content'] = json_obj['human_answers'][0]
+                    human_obj['tokens'] = convert_text_to_tokens(tokenizer, human_obj['content'])
+                    gpt_obj['content'] = json_obj['chatgpt_answers'][0]
+                    gpt_obj['tokens'] = convert_text_to_tokens(tokenizer, gpt_obj['content'])
+                    finance_out_file.write(json.dumps(human_obj) + '\n')
+                    finance_out_file.write(json.dumps(gpt_obj) + '\n')
+                except Exception as e:
+                    print(e)
+        with open('./medicine_token.jsonl', 'a', encoding='utf-8') as medicine_out_file:
+            for line in medicine_file:
+                try:
+                    json_obj = json.loads(line)
+                    human_obj = {}
+                    gpt_obj = {}
+                    human_obj['row_label'] = 0
+                    gpt_obj['row_label'] = 1
+                    human_obj['content'] = json_obj['human_answers'][0]
+                    human_obj['tokens'] = convert_text_to_tokens(tokenizer, human_obj['content'])
+                    gpt_obj['content'] = json_obj['chatgpt_answers'][0]
+                    gpt_obj['tokens'] = convert_text_to_tokens(tokenizer, gpt_obj['content'])
+                    medicine_out_file.write(json.dumps(human_obj) + '\n')
+                    medicine_out_file.write(json.dumps(gpt_obj) + '\n')
+                except Exception as e:
+                    print(e)
+        with open('./wiki_token.jsonl', 'a', encoding='utf-8') as wiki_out_file:
+            for line in wiki_file:
+                try:
+                    json_obj = json.loads(line)
+                    human_obj = {}
+                    gpt_obj = {}
+                    human_obj['row_label'] = 0
+                    gpt_obj['row_label'] = 1
+                    human_obj['content'] = json_obj['human_answers'][0]
+                    human_obj['tokens'] = convert_text_to_tokens(tokenizer, human_obj['content'])
+                    gpt_obj['content'] = json_obj['chatgpt_answers'][0]
+                    gpt_obj['tokens'] = convert_text_to_tokens(tokenizer, gpt_obj['content'])
+                    wiki_out_file.write(json.dumps(human_obj) + '\n')
+                    wiki_out_file.write(json.dumps(gpt_obj) + '\n')
+                except Exception as e:
+                    print(e)
+
 
 if __name__ == "__main__":
-    masked_text = 'Paris is the [MASK] of France.'
-    print(fill_mask(masked_text))
+    # masked_text = 'Paris is the [MASK] of France.'
+    # print(fill_mask(masked_text))
+    convert_hc3_english_to_tokens()
 
