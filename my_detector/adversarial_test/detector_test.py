2a2655e11331b13dd01a22e6b361f0983987ddf8
A-Salty-Fish <1309032417@qq.com>
2024-01-03 21:05:52 +0800

    feat: adversarial train and test init

---
+++ my_detector/adversarial_test/detector_test.py
@@ -0,0 +1,92 @@
+import json
+import os
+import time
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
+import numpy
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def load_detector_config(base_path='./config/', config_name='detector.json'):
+    with open(base_path + config_name, 'r', encoding='utf-8') as text_labels_file:
+        return json.load(text_labels_file)
+
+def load_test_base_model_config(base_path='./config/', config_name='base_model.json'):
+    with open(base_path + config_name, 'r', encoding='utf-8') as text_labels_file:
+        return json.load(text_labels_file)['test']
+
+
+def init_init_model_and_tokenizer(detector_config, base_test_model_config):
+    start_time = time.time()
+
+
+    tokenizer=AutoTokenizer.from_pretrained(base_test_model_config['tokenizer_name'], model_max_length=base_test_model_config['max_length'])
+
+    max_step = -1
+
+    output_dir = detector_config['init']['output_dir']
+
+    for file in os.listdir(output_dir):
+        max_step = max(max_step, int(file.split('-')[1]))
+
+    model_path = output_dir + "/checkpoint-" + str(max_step)
+    print('load model:' + model_path)
+    model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=base_test_model_config['num_labels'])
+
+    print(DEVICE)
+    model = model.to(DEVICE)
+
+    end_time = time.time()
+    print("load model successful: " + str(end_time - start_time))
+    return model, tokenizer
+
+
+def test_accurate(model, tokenizer, train_name, test_name, test_file):
+    start_time = time.time()
+
+    human_correct = 0
+    human_total = 0
+    gpt_correct = 0
+    gpt_total = 0
+    i = 0
+
+    with open(test_file, 'r', encoding='utf-8') as test_input:
+        json_array = json.load(test_input)
+        for json_obj in json_array:
+            i += 1
+            print('test process : %s [%d/%d]' % (str(i*100 / len(json_array)) + '%', i, len(json_array)), end='\r')
+            raw_input = json_obj['content']
+            raw_label = json_obj['label']
+            inputs = tokenizer([raw_input], padding=True, truncation=True, return_tensors="pt").to(DEVICE)
+            outputs = model(**inputs)
+            pred_labels = outputs.logits.cpu().argmax(-1).numpy()
+            pred_label = pred_labels[0]
+
+            if raw_label == 0:
+                human_total += 1
+                if pred_label == 0:
+                    human_correct += 1
+            else:
+                gpt_total += 1
+                if pred_label == 1:
+                    gpt_correct += 1
+
+        end_time = time.time()
+        print("test " + test_name + " successful: " + str(end_time - start_time))
+
+        return {
+            "train_name": train_name,
+            "test_name": test_name,
+            "total_acc": (1.0 * (human_correct + gpt_correct) / (human_total + gpt_total)),
+            "human_acc": (1.0 * human_correct / human_total),
+            "ai_acc": (1.0 * (gpt_correct) / (gpt_total))
+        }
+
+if __name__ == '__main__':
+    detector_config = load_detector_config()
+    base_test_model_config = load_test_base_model_config()
+    model, tokenizer = init_init_model_and_tokenizer(detector_config, base_test_model_config)
+    result = []
+    for name in detector_config['test_files']:
+        result.append(test_accurate(model, tokenizer, 'medicine', name, detector_config['test_files'][name]))
+    print(result)
---
+++ my_detector/adversarial_test/detector_train.py
@@ -0,0 +1,117 @@
+import json
+import time
+
+from datasets import load_dataset
+from sklearn.metrics import f1_score, accuracy_score
+from transformers import TrainingArguments, AutoTokenizer, AutoModelForSequenceClassification, Trainer
+import torch
+
+
+
+# 初始化初始训练配置
+def load_detector_init_train_config(base_path='./config/', config_name='detector.json'):
+    with open(base_path + config_name, 'r', encoding='utf-8') as text_labels_file:
+        return json.load(text_labels_file)['init']
+
+def load_train_base_model_config(base_path='./config/', config_name='base_model.json'):
+    with open(base_path + config_name, 'r', encoding='utf-8') as text_labels_file:
+        return json.load(text_labels_file)['train']
+
+def convert_train_config_to_train_args(train_config):
+    output_dir = train_config['output_dir']
+    num_train_epochs = train_config['num_train_epochs']
+    save_total_limit = train_config['save_total_limit']
+    eval_steps = train_config['eval_steps']
+    save_steps = train_config['save_steps']
+    evaluation_strategy = train_config['evaluation_strategy']
+    training_args = TrainingArguments(output_dir=output_dir, num_train_epochs=num_train_epochs,
+                                      load_best_model_at_end=True,
+                                      save_total_limit=save_total_limit, eval_steps=eval_steps,
+                                      evaluation_strategy=evaluation_strategy,
+                                      save_steps=save_steps)
+    return training_args
+
+
+def load_local_dataset(train_file, test_file):
+    start_time = time.time()
+
+    local_dataset = load_dataset('json', data_files={
+        'train': train_file,
+        'test': test_file,
+    })
+    end_time = time.time()
+    print("load dataset successful: " + str(end_time - start_time))
+    return local_dataset
+
+
+# 加载模型和配置
+def init_base_model_and_tokenizer(base_train_model_config):
+    start_time = time.time()
+    model_name = base_train_model_config['model_name']
+    tokenizer_name = base_train_model_config['tokenizer_name']
+    max_length = base_train_model_config['max_length']
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=max_length)
+    model = AutoModelForSequenceClassification.from_pretrained(tokenizer_name)
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(DEVICE)
+    model = model.to(DEVICE)
+
+    end_time = time.time()
+    print("load model successful: " + str(end_time - start_time))
+    return model, tokenizer
+
+
+def tokenize_data(tokenizer, local_dataset):
+    start_time = time.time()
+
+    def tokenize(batch):
+        return tokenizer(batch["content"], padding=True, truncation=True)
+
+    tokenized_data = local_dataset.map(tokenize, batched=True, batch_size=None)
+    tokenized_data.set_format("torch", columns=["input_ids", "attention_mask", "label"])
+
+    end_time = time.time()
+    print("tokenize dataset successful: " + str(end_time - start_time))
+    return tokenized_data
+
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    f1 = f1_score(labels, preds, average="weighted")
+    acc = accuracy_score(labels, preds)
+    return {"accuracy": acc, "f1": f1}
+
+
+def init_train(base_train_model_config, detector_init_train_config):
+    print("begin init train")
+    start_time = time.time()
+
+    model, tokenizer = init_base_model_and_tokenizer(base_train_model_config)
+
+    train_file = detector_init_train_config['train_file']
+    test_file = detector_init_train_config['test_file']
+    local_dataset = load_local_dataset(train_file, test_file)
+
+    tokenized_data = tokenize_data(tokenizer, local_dataset)
+
+    training_args = convert_train_config_to_train_args(train_config=detector_init_train_config)
+
+    trainer = Trainer(model=model, args=training_args, compute_metrics=compute_metrics,
+                      train_dataset=tokenized_data["train"],
+                      eval_dataset=tokenized_data["train"]
+                      )
+
+    end_time = time.time()
+    print("train init successful " + " : " + str(end_time - start_time))
+    trainer.train()
+
+
+if __name__ == '__main__':
+    # print(load_detector_init_train_config())
+    # print(convert_train_config_to_train_args(load_detector_init_train_config()))
+
+    base_model_config = load_train_base_model_config()
+    detector_init_train_config = load_detector_init_train_config()
+    init_train(base_model_config, detector_init_train_config)
