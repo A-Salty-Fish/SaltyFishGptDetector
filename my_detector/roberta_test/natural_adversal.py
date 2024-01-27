06c97f44502360ea8d1da93302dc2195fd2a684a
A-Salty-Fish <1309032417@qq.com>
2024-01-27 20:15:04 +0800

    feat: fullfil natural adversal module

--- my_detector/roberta_test/natural_adversal.py
+++ my_detector/roberta_test/natural_adversal.py
@@ -1,6 +1,12 @@
+import datetime
 import json
+import os
+import random
 import time
+import math
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from torch import nn
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -41,14 +47,14 @@ class MyAdversaryDataset(Dataset):
     def __init__(self, datas: list, tokenizer):
 
         texts = [x['content'] for x in datas]
-        print("begin tokenize datas")
+        # print("begin tokenize datas")
         self.texts = [tokenizer(text, padding='max_length',
                                 max_length=256,
                                 truncation=True,
                                 return_tensors="pt")
                       for text in texts]
 
-        print("end tokenize datas")
+        # print("end tokenize datas")
         self.labels = [x['label'] for x in datas]
 
         self.domains = []
@@ -133,7 +139,18 @@ def adversary_val(train_model: MyClassifier, prompt_dataloader_map, device='cuda
     return prompt_loss_map
 
 
+
+
 class AdversaryGenerator:
+
+    def init_adversary_model_and_tokenizer(self, model_name="mistralai/Mistral-7B-Instruct-v0.2"):
+        start_time = time.time()
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
+
+        end_time = time.time()
+        print("load adversal generator model successful: " + str(end_time - start_time))
+        return model, tokenizer
 
     # 默认的文件加载函数
     def default_file_load_func(self, file):
@@ -157,12 +174,39 @@ class AdversaryGenerator:
         return file_objs
 
     # 初始化分类好的验证集，模型等参数
-    def __init__(self, chat_model, chat_tokenizer, local_val_files_map, file_load_func=default_file_load_func):
+    def __init__(self,  local_val_files_map, prompts_map, train_df, file_load_func=default_file_load_func, tmp_data_path='./tmp_ad/', lazy_init=False):
         print("begin init adversary generator")
         begin_time = time.time()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.chat_model = chat_model
-        self.chat_tokenizer = chat_tokenizer
+
+        self.lazy_init = lazy_init
+        if not lazy_init:
+            chat_model, chat_tokenizer = self.init_adversary_model_and_tokenizer()
+            self.chat_model = chat_model
+            self.chat_tokenizer = chat_tokenizer
+        else:
+            self.chat_model = None
+            self.chat_tokenizer = None
+
+        self.tmp_data_path = tmp_data_path
+        # Check if the directory exists
+        if not os.path.exists(tmp_data_path):
+            # Create the directory
+            os.makedirs(tmp_data_path)
+            print("Directory created successfully: " + tmp_data_path)
+        else:
+            print("Directory already exists: " + tmp_data_path)
+
+        self.prompts_map = prompts_map
+
+        train_texts = [x['content'] for i, x in train_df.iterrows()]
+        train_labels = [x['label'] for i, x in train_df.iterrows()]
+        self.train_datas = []
+        for i in range(0, len(train_texts)):
+            self.train_datas.append({
+                'label': train_labels[i],
+                'content': train_texts[i]
+            })
 
         self.val_data_map = {}
         for key in local_val_files_map:
@@ -171,7 +215,7 @@ class AdversaryGenerator:
         if file_load_func is not None:
             for key in local_val_files_map:
                 file = local_val_files_map[key]
-                file_objs = file_load_func(file)
+                file_objs = file_load_func(self, file)
                 self.val_data_map[key] += file_objs
 
         end_time = time.time()
@@ -198,12 +242,85 @@ class AdversaryGenerator:
             key_loss_map[key] = key_loss
         return key_loss_map
 
+    def calculate_adversary_data_num_by_loss(self, loss_map):
+        batch_num = 16
+
+        log_loss_map = {}
+        log_loss_sum = 0
+        log_loss_percent_map = {}
+
+        for k in loss_map:
+            log_loss_map[k] = (-math.log10(1 - loss_map[k]))
+            log_loss_sum += log_loss_map[k]
+
+        for k in log_loss_map:
+            log_loss_percent_map[k] = log_loss_map[k] / log_loss_sum
+
+        sorted_dict = dict(sorted(log_loss_percent_map.items(), key=lambda item: item[1], reverse=True))
+        sorted_list = [[k, sorted_dict[k]] for k in sorted_dict]
+
+        adversary_nums_map = {}
+
+        for key in loss_map:
+            adversary_nums_map[key] = 1
+
+        for kv in sorted_list:
+            adversary_nums_map[kv[0]] += int(0.5 + kv[1] * (batch_num - len(loss_map)))
+
+        return adversary_nums_map
+
+    def generate_adversary_train_data_by_val_result(self, key_loss_map, batch_size=16):
+        begin_time = time.time()
+
+        if self.lazy_init:
+            if self.chat_model is None or self.chat_tokenizer is None:
+                self.chat_model, self.chat_tokenizer = self.init_adversary_model_and_tokenizer()
+                self.lazy_init = False
+
+        adversary_nums_map = self.calculate_adversary_data_num_by_loss(key_loss_map)
+
+        prompts_map = self.prompts_map
+        tmp_data_path = self.tmp_data_path
+        save_file_name = 'adversary_train_data' + str(datetime.datetime.now()) + '.jsonl'
+
+        row_train_datas = [x for x in self.train_datas if x['label'] == 1][0: int(batch_size*1.5)]
+        row_data_index = 0
+        random.shuffle(row_train_datas)
+
+        adversary_train_datas = []
+
+        for key in adversary_nums_map:
+            print('generate process : [%d/%d]' % (row_data_index, batch_size), end='\r')
+            prompt_template = prompts_map[key]
+            for i in range(0, adversary_nums_map[key]):
+                row_train_data = row_train_datas[row_data_index]
+                chat_text = prompt_template + row_train_data['content']
+                chat_result = self.generate_chat_context(chat_text)
+                adversary_train_datas.append({
+                    'label': 1,
+                    'content': chat_result,
+                    'prompt': key
+                })
+                row_data_index += 1
+
+        print('generate finish: ' + str(time.time() - begin_time))
+
+        random.shuffle(adversary_train_datas)
+        # 截断
+        adversary_train_datas = adversary_train_datas[0: batch_size]
+
+        with open(tmp_data_path + save_file_name, 'w', encoding='utf-8') as out_f:
+            for data in adversary_train_datas:
+                out_f.write(json.dumps(data) + '\n')
+
+        return adversary_train_datas
+
+
     def generate_chat_context(self, text):
         return chat(self.chat_model, self.chat_tokenizer, text)
 
 
 if __name__ == '__main__':
-    model, tokenizer = init_model_and_tokenizer()
     local_val_file_map = {
         'rewrite': '../../data_collector/test_data/hc3_english_mix_multi/open_qa.rewrite.mix.jsonl',
         'continue': '../../data_collector/test_data/hc3_english_mix_multi/open_qa.continue.mix.jsonl',
@@ -212,8 +329,32 @@ if __name__ == '__main__':
         'easy': '../../data_collector/test_data/hc3_english_mix_multi/open_qa.easy.mix.jsonl',
         'qa': '../../data_collector/test_data/hc3_english_mix_multi/open_qa.mix.jsonl',
     }
-    adversary_generator = AdversaryGenerator(model, tokenizer, local_val_file_map)
+    prompts_map = {
+        'rewrite': 'Please rewrite the following content, {without any useless content}:',
+        'continue': 'Please continue to write the following content, {without any useless content}:',
+        'easy': 'Please change the following content to make it easier to understand, {without any useless content}:',
+        'academic': 'Please change the following content to be more academic and professional, {without any useless content}:',
+        'difficult': 'Please change the following content to make it more difficult to understand, {without any useless content}:',
+        'qa': 'The following is a response to a question, please re-answer the question based on this response, {without any useless content}:'
+    }
+
+    train_file = pd.read_json("../Deberta_test/data/hc3_all.jsonl.train")
+    train_df, val_df = train_test_split(train_file, test_size=0.2, random_state=0)
+
+    adversary_generator = AdversaryGenerator(
+        local_val_files_map=local_val_file_map,
+        prompts_map=prompts_map,
+        train_df=train_df
+    )
 
     train_model, train_tokenizer = init_test_model_and_tokenizer(test_model_path='hc3_row.pt')
 
-    print(adversary_generator.val_cur_train_model_by_local_datas(cur_train_model=train_model, cur_train_tokenizer=train_tokenizer))
+    loss_map = adversary_generator.val_cur_train_model_by_local_datas(cur_train_model=train_model, cur_train_tokenizer=train_tokenizer)
+    print(loss_map)
+
+    data_num_map = adversary_generator.calculate_adversary_data_num_by_loss(loss_map)
+    print(data_num_map)
+
+    print(adversary_generator.generate_adversary_train_data_by_val_result(loss_map))
+
+
--- other_detectors/tmp_json.py
+++ other_detectors/tmp_json.py
@@ -3,45 +3,76 @@ import json
 import os
 
 if __name__ == '__main__':
-    import pandas as pd
-    knn_path = 'D:\\毕设\\数据\\实验结果\\KNN'
-    all_files = os.listdir(knn_path)
+    loss_map = {'rewrite': 0.4930301308631897, 'continue': 0.2966940999031067, 'academic': 0.4215707778930664,
+                'difficult': 0.993345320224762, 'easy': 0.3965831696987152, 'qa': 0.29204198718070984}
 
-    print(all_files)
+    import math
 
-    file_3 = [x for x in all_files if x.find('_3') != -1]
-    file_5 = [x for x in all_files if x.find('_5') != -1]
-    file_7 = [x for x in all_files if x.find('_7') != -1]
-    file_9 = [x for x in all_files if x.find('_9') != -1]
-    file_11 = [x for x in all_files if x.find('_11') != -1]
+    batch_num = 16
 
-    print(file_3)
-    print(file_5)
-    print(file_7)
-    print(file_9)
-    print(file_11)
-    # Specify the CSV files to be merged
-    # csv_files = ['file1.csv', 'file2.csv', 'file3.csv']
+    log_loss_map = {}
+    log_loss_sum = 0
+    log_loss_percent_map = {}
+
+    for k in loss_map:
+        log_loss_map[k] = (-math.log10(1 - loss_map[k]))
+        log_loss_sum += log_loss_map[k]
+
+    for k in log_loss_map:
+        log_loss_percent_map[k] = log_loss_map[k] / log_loss_sum
+
+    sorted_dict = dict(sorted(log_loss_percent_map.items(), key=lambda item: item[1], reverse=True))
+    sorted_list = [[k, sorted_dict[k]] for k in sorted_dict]
+
+    adversary_nums_map = {}
+
+    for key in loss_map:
+        adversary_nums_map[key] = 1
+
+    for kv in sorted_list:
+        adversary_nums_map[kv[0]] += int(0.5 + kv[1] * (batch_num - len(loss_map)))
+
+    print(adversary_nums_map)
+
+    # import pandas as pd
+    # knn_path = 'D:\\毕设\\数据\\实验结果\\KNN'
+    # all_files = os.listdir(knn_path)
     #
-    # # Read each CSV file into a DataFrame
-
-    dfs_3 = [pd.read_csv(knn_path + '\\' + file) for file in file_3]
-    dfs_5 = [pd.read_csv(knn_path + '\\' + file) for file in file_5]
-    dfs_7 = [pd.read_csv(knn_path + '\\' + file) for file in file_7]
-    dfs_9 = [pd.read_csv(knn_path + '\\' + file) for file in file_9]
-    dfs_11 = [pd.read_csv(knn_path + '\\' + file) for file in file_11]
+    # print(all_files)
     #
-    # # Concatenate the DataFrames
-    # df_merged = pd.concat(dfs)
+    # file_3 = [x for x in all_files if x.find('_3') != -1]
+    # file_5 = [x for x in all_files if x.find('_5') != -1]
+    # file_7 = [x for x in all_files if x.find('_7') != -1]
+    # file_9 = [x for x in all_files if x.find('_9') != -1]
+    # file_11 = [x for x in all_files if x.find('_11') != -1]
     #
-    # # Save the merged DataFrame to a new CSV file
-    # df_merged.to_csv('merged_file.csv', index=False)
-
-    pd.concat(dfs_3).to_csv(knn_path + '\\' + 'merged_3.csv', index=False)
-    pd.concat(dfs_5).to_csv(knn_path + '\\' + 'merged_5.csv', index=False)
-    pd.concat(dfs_7).to_csv(knn_path + '\\' + 'merged_7.csv', index=False)
-    pd.concat(dfs_9).to_csv(knn_path + '\\' + 'merged_9.csv', index=False)
-    pd.concat(dfs_11).to_csv(knn_path + '\\' + 'merged_11.csv', index=False)
+    # print(file_3)
+    # print(file_5)
+    # print(file_7)
+    # print(file_9)
+    # print(file_11)
+    # # Specify the CSV files to be merged
+    # # csv_files = ['file1.csv', 'file2.csv', 'file3.csv']
+    # #
+    # # # Read each CSV file into a DataFrame
+    #
+    # dfs_3 = [pd.read_csv(knn_path + '\\' + file) for file in file_3]
+    # dfs_5 = [pd.read_csv(knn_path + '\\' + file) for file in file_5]
+    # dfs_7 = [pd.read_csv(knn_path + '\\' + file) for file in file_7]
+    # dfs_9 = [pd.read_csv(knn_path + '\\' + file) for file in file_9]
+    # dfs_11 = [pd.read_csv(knn_path + '\\' + file) for file in file_11]
+    # #
+    # # # Concatenate the DataFrames
+    # # df_merged = pd.concat(dfs)
+    # #
+    # # # Save the merged DataFrame to a new CSV file
+    # # df_merged.to_csv('merged_file.csv', index=False)
+    #
+    # pd.concat(dfs_3).to_csv(knn_path + '\\' + 'merged_3.csv', index=False)
+    # pd.concat(dfs_5).to_csv(knn_path + '\\' + 'merged_5.csv', index=False)
+    # pd.concat(dfs_7).to_csv(knn_path + '\\' + 'merged_7.csv', index=False)
+    # pd.concat(dfs_9).to_csv(knn_path + '\\' + 'merged_9.csv', index=False)
+    # pd.concat(dfs_11).to_csv(knn_path + '\\' + 'merged_11.csv', index=False)
 
     # department_map = {
     #
