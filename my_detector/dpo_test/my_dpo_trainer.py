a1e6e8944ae6ec597a6f44f9597c1ab0fb501b40
A-Salty-Fish <1309032417@qq.com>
2024-02-03 21:03:14 +0800

    fix: init dpo train pipe

--- my_detector/dpo_test/my_dpo_trainer.py
+++ my_detector/dpo_test/my_dpo_trainer.py
@@ -3,11 +3,24 @@ import json
 import time
 from typing import Dict
 
+import datasets
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
+from peft import LoraConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, BitsAndBytesConfig
 from datasets import Dataset
 from trl import DPOTrainer
 from torch.nn.utils.rnn import pad_sequence
+
+peft_config = LoraConfig(
+    target_modules=["q_proj", "k_proj"],
+    init_lora_weights=False
+)
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
 
 
 def load_trainer_args():
@@ -24,7 +37,7 @@ def load_trainer_args():
     # --remove_unused_columns false \
     # --logging_steps 10 \
     # --output_dir ./weights/DPO_BC
-    train_args = TrainingArguments()
+    train_args = TrainingArguments(output_dir='./tmp.pt')
     train_args.gradient_accumulation_steps = 1
     train_args.num_train_epochs = 2
     train_args.save_steps = 200
@@ -34,7 +47,8 @@ def load_trainer_args():
     train_args.ddp_find_unused_parameters = False
     train_args.remove_unused_columns = False
     train_args.logging_steps = 100
-    train_args.output_dir = './tmp.pt'
+    train_args.per_device_train_batch_size = 2
+    train_args.per_device_eval_batch_size = 2
     return train_args
 
 
@@ -42,17 +56,19 @@ def load_model(model_name="mistralai/Mistral-7B-Instruct-v0.2"):
     all_begin_time = time.time()
 
     begin_time = time.time()
-    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16,
-                                                 trust_remote_code=True).to("cuda:0")
+    model = AutoModelForCausalLM.from_pretrained(model_name,
+                                                 quantization_config=bnb_config,
+                                                 trust_remote_code=True)
     print("load model success: " + str(time.time() - begin_time))
 
     begin_time = time.time()
-    ref_model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16,
-                                                     trust_remote_code=True).to("cuda:0")
+    ref_model = AutoModelForCausalLM.from_pretrained(model_name,
+                                                     quantization_config = bnb_config,
+                                                     trust_remote_code=True)
     print("load ref_model success: " + str(time.time() - begin_time))
 
     begin_time = time.time()
-    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     print("load tokenizer success: " + str(time.time() - begin_time))
 
     print("load all success: " + str(time.time() - all_begin_time))
@@ -123,32 +139,33 @@ def tokenize_batch_element(prompt: str, chosen: str, rejected: str, truncation_m
     return batch
 
 
-def collate_fn(batch, tokenizer):
-    # first, pad everything to the same length
-    tokenizer.pad_token_id = tokenizer.eod_id
-    padded_batch = {}
-    for k in batch[0].keys():
-        if k.endswith('_input_ids') or k.endswith('_attention_mask') or k.endswith('_labels'):
-            if 'prompt' in k:  # adapted from https://stackoverflow.com/questions/73256206
-                to_pad = [torch.LongTensor(ex[k][::-1]) for ex in batch]
-            else:
-                to_pad = [torch.LongTensor(ex[k]) for ex in batch]
-            if k.endswith('_input_ids'):
-                padding_value = tokenizer.pad_token_id
-            elif k.endswith('_labels'):
-                padding_value = -100
-            elif k.endswith('_attention_mask'):
-                padding_value = 0
-            else:
-                raise ValueError(f"Unexpected key in batch '{k}'")
-
-            padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
-            if 'prompt' in k:  # for the prompt, flip back so padding is on left side
-                padded_batch[k] = padded_batch[k].flip(dims=[1])
-        else:
-            padded_batch[k] = [ex[k] for ex in batch]
-
-    return padded_batch
+#
+# def collate_fn(batch, tokenizer):
+#     # first, pad everything to the same length
+#     tokenizer.pad_token_id = tokenizer.eod_id
+#     padded_batch = {}
+#     for k in batch[0].keys():
+#         if k.endswith('_input_ids') or k.endswith('_attention_mask') or k.endswith('_labels'):
+#             if 'prompt' in k:  # adapted from https://stackoverflow.com/questions/73256206
+#                 to_pad = [torch.LongTensor(ex[k][::-1]) for ex in batch]
+#             else:
+#                 to_pad = [torch.LongTensor(ex[k]) for ex in batch]
+#             if k.endswith('_input_ids'):
+#                 padding_value = tokenizer.pad_token_id
+#             elif k.endswith('_labels'):
+#                 padding_value = -100
+#             elif k.endswith('_attention_mask'):
+#                 padding_value = 0
+#             else:
+#                 raise ValueError(f"Unexpected key in batch '{k}'")
+#
+#             padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
+#             if 'prompt' in k:  # for the prompt, flip back so padding is on left side
+#                 padded_batch[k] = padded_batch[k].flip(dims=[1])
+#         else:
+#             padded_batch[k] = [ex[k] for ex in batch]
+#
+#     return padded_batch
 
 
 def load_dataset(data_path, tokenizer, prompt_key='prompt', accept_key='accept', reject_key='reject'):
@@ -170,31 +187,59 @@ def load_dataset(data_path, tokenizer, prompt_key='prompt', accept_key='accept',
         yield tokenize_data
 
 
+def prepare_train_data():
+    json_objs = []
+    with open('../../data_collector/test_data/hc3_english_mix_multi/wiki_csai.mix.jsonl', 'r',
+              encoding='utf-8') as in_f:
+        for line in in_f:
+            json_objs.append(json.loads(line))
+    out_json_objs = [
+        {
+            'prompt': x['question'],
+            'chosen': x['human'],
+            'rejected': x['ai']
+        }
+        for x in json_objs
+    ]
+    with open('./data/wiki_csai.mix.train', 'w', encoding='utf-8') as out_f:
+        out_f.write(json.dumps(out_json_objs))
+
+
 if __name__ == '__main__':
+    # prepare_train_data()
+
     train_args = load_trainer_args()
 
-    model, ref_model, tokenizer = load_model()
+    train_dataset = datasets.load_dataset('json', data_files={'train': './data/wiki_csai.mix.train'})['train']
 
-    dataset = Dataset.from_generator(
-        lambda: load_dataset(
-            '../../data_collector/test_data/hc3_english_mix_multi/wiki_csai.mix.jsonl',
-            tokenizer,
-            prompt_key='question',
-            accept_key='human',
-            reject_key='ai'
-        )
-    )
+    model, ref_model, tokenizer = load_model("microsoft/phi-2")
+    tokenizer.pad_token = tokenizer.eos_token
+
+    # tarin_dataset = Dataset.from_generator(
+    #     lambda: load_dataset(
+    #         '../../data_collector/test_data/hc3_english_mix_multi/wiki_csai.mix.jsonl',
+    #         tokenizer,
+    #         prompt_key='question',
+    #         accept_key='human',
+    #         reject_key='ai'
+    #     )
+    # )
 
     # note: use gradient checkpointing to save memory at the expense of slower backward pass.
-    model.gradient_checkpointing_enable()
-    model.enable_input_require_grads()
+    # model.gradient_checkpointing_enable()
+    # model.enable_input_require_grads()
 
-    ref_model = ref_model.eval().requires_grad_(False)
+    # ref_model = ref_model.eval().requires_grad_(False)
     print(train_args)
-    trainer = DPOTrainer(model, ref_model, args=train_args, train_dataset=dataset,
-                         data_collator=functools.partial(collate_fn, tokenizer=tokenizer)
+    trainer = DPOTrainer(model, ref_model, args=train_args, train_dataset=train_dataset,
+                         # data_collator=functools.partial(collate_fn, tokenizer=tokenizer),
+                         tokenizer=tokenizer,
+                         max_length=512,
+                         max_prompt_length=512,
+                         peft_config=peft_config
                          )
 
     trainer.train()
     trainer.save_model(train_args.output_dir)
 
+    pass
