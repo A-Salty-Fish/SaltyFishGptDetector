f6c69ca04d055305cef075ebf76dd9f39dae5891
A-Salty-Fish <1309032417@qq.com>
2024-02-17 12:44:21 +0800

    fix: train data type

--- my_detector/dpo_test/my_dpo_trainer.py
+++ my_detector/dpo_test/my_dpo_trainer.py
@@ -352,6 +352,7 @@ def convert_dataset(file_path):
         for json_obj in json_objs:
             new_jsons.append(
                 {
+                    'prompt': '<s>[INST] ' + json_obj['prompt'] + ' [/INST]',
                     'chosen': '<s>[INST] ' + json_obj['prompt'] + ' [/INST]' + json_obj['chosen'] + '</s>',
                     'rejected': '<s>[INST] ' + json_obj['prompt'] + ' [/INST]' + json_obj['rejected'] + '</s>'
                 }
