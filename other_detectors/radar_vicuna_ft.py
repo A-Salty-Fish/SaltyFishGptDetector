03c5232d999f65f41770db4da6659eb91d81deda
A-Salty-Fish <1309032417@qq.com>
2024-03-07 16:31:00 +0800

    fix: radar and hc3 fine tune

--- other_detectors/radar_vicuna_ft.py
+++ other_detectors/radar_vicuna_ft.py
@@ -4,7 +4,7 @@ import time
 from datasets import load_dataset
 # fine tune the radar vicuna
 
-from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, AutoTokenizer
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, AutoTokenizer, pipeline
 from sklearn.metrics import accuracy_score, f1_score
 
 def init_model_and_tokenizer():
@@ -83,10 +83,38 @@ def prepare_train_data():
             out_f.write(json.dumps(all_json_objs))
 
 
+def init_classifier(model_path='./radar_vicuna_ft/checkpoint-3055/'):
+    classifier = pipeline("sentiment-analysis", model=model_path,
+                          tokenizer=AutoTokenizer.from_pretrained("TrustSafeAI/RADAR-Vicuna-7B", trust_remote_code=True,
+                                                                  max_length=512))
+    return classifier
+
+
+def classify_is_human(classifier, text, bar=0.5):
+
+    raw_output = classifier(text)[0]
+    if raw_output['label'] == 'LABEL_0':
+        return raw_output['score'] >= bar
+    else:
+        return raw_output['score'] < bar
+
+
+
 if __name__ == '__main__':
 
-    model, tokenizer = init_model_and_tokenizer()
-    prepare_train_data()
-    train(model, tokenizer, './radar_vicuna_ft.train', 'radar_vicuna_ft')
+    # model, tokenizer = init_model_and_tokenizer()
+    # prepare_train_data()
+    # train(model, tokenizer, './radar_vicuna_ft.train', 'radar_vicuna_ft')
 
+    # model, tokenizer = init_test_model_and_tokenizer('./radar_vicuna_ft/checkpoint-3055/')
+    human_text = "I know this question has a lot of answers already, but I feel the answers are phrased either strongly against, or mildly for, co-signing. What it amounts down to is that this is a personal choice. You cannot receive reliable information as to whether or not co-signing this loan is a good move due to lack of information. The person involved is going to know the person they would be co-signing for, and the people on this site will only have their own personal preferences of experiences to draw from. You know if they are reliable, if they will be able to pay off the loan without need for the banks to come after you.  This site can offer general theories, but I think it should be kept in mind that this is wholly a personal decision for the person involved, and them alone to make based on the facts that they know and we do not."
+    ai_text = "Co-signing a personal loan for a friend or family member can be a risky proposition. When you co-sign a loan, you are agreeing to be responsible for the loan if the borrower is unable to make the payments. This means that if your friend or family member defaults on the loan, you will be on the hook for the remaining balance.There are a few things to consider before co-signing a personal loan for someone:Do you trust the borrower to make the payments on time and in full? If you are not confident that the borrower will be able to make the payments, it may not be a good idea to co-sign the loan.Can you afford to make the payments if the borrower defaults? If you are unable to make the payments, co-signing the loan could put your own financial stability at risk.What is the purpose of the loan? If the borrower is using the loan for a risky or questionable venture, it may not be worth the risk to co-sign.Is there another way for the borrower to get the loan without a co-signer? If the borrower has a good credit score and is able to qualify for a loan on their own, it may not be necessary for you to co-sign.In general, it is important to carefully consider the risks and potential consequences before co-signing a loan for someone. If you do decide to co-sign, it is a good idea to have a conversation with the borrower about their plans for making the loan payments and to have a clear understanding of your responsibilities as a co-signer."
+
+    classifier = init_classifier()
+    print(classifier(human_text))
+    print(classifier(ai_text))
+
+    print(classify_is_human(classifier, human_text))
+    print(classify_is_human(classifier, ai_text))
+    # classify_is_human(model, tokenizer, "hello, my name is chatgpt. what can i help you. ")
     pass
