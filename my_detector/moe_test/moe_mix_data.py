e61c7b40f9d56364921221a6b49a64d38fffb117
A-Salty-Fish <1309032417@qq.com>
2024-03-23 19:46:00 +0800

    feat: mix adversary data

--- my_detector/moe_test/moe_mix_data.py
+++ my_detector/moe_test/moe_mix_data.py
@@ -17,20 +17,23 @@ def convert_jsonl_to_train_test(jsonl_file, train_rate=0.2):
             json_objs.append(
                 {
                     'label': 1,
-                    'content': json_obj['ai']
+                    'content': json_obj['ai_rewrite']
                 }
             )
     with open(jsonl_file + '.train', 'w', encoding='utf-8') as train_f:
         train_f.write(json.dumps(json_objs[0: int(train_rate*len(json_objs))]))
-    with open(jsonl_file + '.test', 'w', encoding='utf-8') as train_f:
+    with open(jsonl_file + '.all.test', 'w', encoding='utf-8') as train_f:
         train_f.write(json.dumps(json_objs[int(train_rate*len(json_objs)):]))
 
 
 if __name__ == '__main__':
     dirs = [
-        './data/nature/glm/',
-        './data/nature/mix/',
-        './data/nature/qwen/'
+        # './data/nature/glm/',
+        # './data/nature/mix/',
+        # './data/nature/qwen/'
+        './data/adversary/dp/',
+        './data/adversary/dpo/',
+        './data/adversary/qwen/',
     ]
     for dir in dirs:
         for file in os.listdir(dir):
